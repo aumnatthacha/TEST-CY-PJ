dd537aa81d22909cea5af744c7f757003914cc98
aumnatthacha <aummumdaeng@gmail.com>
2024-07-12 20:30:00 +0700

    update_addDashboard

--- cypress/e2e/addDashboard.cy.js
+++ cypress/e2e/addDashboard.cy.js
@@ -4,42 +4,31 @@ describe("Add Dashboard Test", () => {
   it("should login successfully with correct credentials and perform device operations", () => {
     login("AumNatthacha", "Natthacha2002");
 
-    // cy.wait(2000);
-
-    //Add Dashboard
+    // Click on Dashboard link
     cy.get("#dashboard-nav-link-sidebar").click();
-    // cy.get("#dashboard-nav-link-sidebar").click();
     cy.contains("Dashboard").click();
+
+    // Add Dashboard
     cy.get("#toggle-add-dashboard-dialog-btn").click();
     cy.get("#nameDashboard").type("Sensor_01");
-
-    cy.get("#description").type(
-      "แดชบอร์ดนี้ใช้ในการตรวจสอบข้อมูลจาก Sensor_01 เช่น อุณหภูมิและความชื้น",
-      { force: true }
-    );
+    cy.get("#description").type("แดชบอร์ดนี้ใช้ในการตรวจสอบข้อมูลจาก Sensor_01 เช่น อุณหภูมิและความชื้น", { force: true });
     cy.get("#submit-add-dashboard-btn").click();
-
     cy.contains("Your Sensor_01 dashboard has been added").should("be.visible");
-
     cy.contains("Sensor_01").should("be.visible");
     cy.screenshot();
 
-    //Edit
-    cy.get("#edit-dashboard-option-66912dc2fed9b7834140dedc").click();
+    // Edit Dashboard
+    cy.get("#edit-dashboard-option-66912f56fed9b7834140df19").click();
     cy.get("#nameDashboard").clear().type("Sensor_02");
-    cy.get("#description")
-      .clear({ force: true })
-      .type("อุณหภูมิและความชื้น", { force: true });
+    cy.get("#description").clear({ force: true }).type("อุณหภูมิและความชื้น", { force: true });
     cy.get("#submit-edit-dashboard-btn").click();
     cy.contains("updated your dashboard").should("be.visible");
     cy.screenshot();
 
-    //DELETED
-    cy.get("#delete-dashboard-option-66912dc2fed9b7834140dedc").click();
-    cy.wait(2000);
+    // Delete Dashboard
+    cy.get("#delete-dashboard-option-66912f56fed9b7834140df19").click();
     cy.get("#confirm-delete-dashboard-btn").click();
     cy.contains("deleted your dashboard").should("be.visible");
-
     cy.screenshot();
   });
 });
